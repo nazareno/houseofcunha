15fc19f756e3eec37ae35dcf127f155d36d472c4
Nazareno <nazareno@computacao.ufcg.edu.br>
2015-05-31 17:40:56 -0300

    Salvando mais cunhometro

--- rmarkdown/acunha.R
+++ rmarkdown/acunha.R
@@ -45,14 +45,30 @@ ggplot(tops, aes(reorder(nome,prop), prop*100)) +
 dev.off()
   
 
-ac$nivel = factor(ac$nivel, levels = c('Cunha', 'Muro', 'Acunha'), ordered = T)
 to_plot = mutate(ac, cat_partido = ifelse(partido == 'pt' | partido == 'pmdb' | partido == 'psol' 
                                 | partido == 'psdb' , as.character(partido), 'outros'))
-
-ggplot(to_plot, aes(cat_partido, prop, colour = cat_partido) ) + 
-  geom_point( position = position_jitter(), alpha = 0.5, size = 4 ) +
+to_plot$cat_partido <- factor(to_plot$cat_partido, 
+                              levels = c("psol", "psdb", "pt", "pmdb", "outros"),
+                              ordered = T)
+                              
+require(scales)
+png(file="cunhometro-por-partido.png", height = 450, width = 600)
+ggplot(to_plot, aes(cat_partido, prop * 100, colour = cat_partido) ) + 
+  geom_point( position = position_jitter(width = 0.2), size = 5 ) +
+  scale_colour_manual(values = c(alpha("#E69F00", 0.6),
+                                 alpha("#0066CC", 0.6),
+                                 alpha("#FF3300", 0.6),
+                                 alpha("darkred", 0.6),
+                                 alpha("grey70", .3)), 
+                      guide = guide_legend(title = "partido", 
+                                           override.aes = list(alpha = 1, size = 4))) + 
+  theme(axis.title = element_text(color="#666666", face="bold", size=18), 
+        axis.text.y = element_text(size=18), 
+        axis.line = element_blank()) + 
+  xlab("Partido") + ylab("Concordância com Cunha (%)") + 
   coord_flip() + 
   theme_bw()
+dev.off()
 
 # top do bem
 png(file="tops-acunhas.png", height = 850, width = 650)
