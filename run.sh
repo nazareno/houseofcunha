4d92b23efa96a6bf691ddc0c4ad4ba03ec84acfd
andryw <andryw.neo@gmail.com>
2015-07-19 11:05:40 -0300

    select key polls

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "GETTING THE DATA"
-sh get_data.sh
+sh get_data2.sh
 echo "DOWNLOAD FINISH. NOW WILL TRANSFORM THE DATA"
-python parser.py "votacoes/*.xml" "votacoes.csv"
+python parser.py "votacoes2/*.xml" "votacoes2.csv"
 echo "THE END"
