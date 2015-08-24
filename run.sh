3e4c6ba58be4debb67e0d7f9e0632cb9908cb633
andryw <hatitorluvit000>
2015-08-24 14:24:56 -0300

    merge

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "GETTING THE DATA"
-sh get_data2.sh
+sh get_data.sh
 echo "DOWNLOAD FINISH. NOW WILL TRANSFORM THE DATA"
-python parser.py "votacoes2/*.xml" "votacoes2.csv"
+python parser.py "votacoes/*.xml" "votacoes.csv"
 echo "THE END"
