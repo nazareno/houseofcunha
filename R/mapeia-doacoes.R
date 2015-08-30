11e3bb3241c62e5d13afdeca1a00216287277fe5
Nazareno <nazareno@computacao.ufcg.edu.br>
2015-08-30 14:01:07 -0300

    Cria 1a versão do mapa de empresas top doadoras

--- R/mapeia-doacoes.R
+++ R/mapeia-doacoes.R
@@ -1,34 +1,90 @@
 #
 # Gera um mapa dos deputaods a partir das doações recebidas pelos deputados 
 # 
-require(dplyr)
 require(reshape2)
 require(ggplot2)
 require(FactoMineR)
+require(dplyr)
 source("R/camara-lib.R")
 
 doacoes.todas <- ler_doacoes_de_eleitos(arquivo.doacoes = "data//receitas_todos_deputados_federais.txt", 
                                         arquivo.eleitos = "deputados-detalhes.csv")
+# Mapeando CPF/CNPJ -> Nome único (o 1o que acontece)
+doacoes.todas <- adiciona_nomes_corrigidos(doacoes.todas)
 
+###############
+# Descritivo
+###############
 
+# CNPJs das direções e comitês
+cnpjs.comites <- doacoes.todas %>% 
+  filter(Nome.do.doador %in% c('Direção Nacional', 
+                               'Direção Estadual/Distrital', 
+                               'Direção Municipal', 
+                               'Comitê Financeiro Nacional para Presidente da República',
+                               'Comitê Financeiro Único')) %>%
+  select(CPF.CNPJ.do.doador) %>% 
+  unique()
 
+total <- doacoes.todas %>% summarise(doado  = sum(valor.doacoes))
+
+doacoes.comites <- doacoes.todas %>% 
+  filter(CPF.CNPJ.do.doador %in% cnpjs.comites$CPF.CNPJ.do.doador) %>% 
+  group_by(CPF.CNPJ.do.doador, Nome.doador.corrigido) %>% 
+  summarise(doado = sum(valor.doacoes))
+
+print(paste0("Proporção das doações que vem de comitês e diretórios: ", sum(doacoes.comites$doado) / sum(total$doado)))
+
+ggplot(doacoes.comites, aes(x = reorder(Nome.doador.corrigido, doado), y = doado / 1e6)) + 
+  geom_bar(stat='identity') + 
+  theme_bw() + 
+  scale_y_continuous(labels=function(n){format(n, scientific = FALSE)}) + 
+  ylab('Milhões doados') + 
+  xlab('') + 
+  coord_flip()
+
+## Doações de empresas: 
 doacoes.empresas <- doacoes.todas %>%
-  filter((! Nome.do.doador %in% c('Direção Nacional', 
-                                  'Direção Estadual/Distrital', 
-                                  'Direção Municipal', 
-                                  'Comitê Financeiro Nacional para Presidente da República',
-                                  'Comitê Financeiro Único')) &
-           as.numeric(as.character(CPF.CNPJ.do.doador)) > 100000000000) %>% 
-  group_by(Nome.candidato, Nome.do.doador, CPF.CNPJ.do.doador, Sigla..Partido, UF) %>% 
+  filter(! (CPF.CNPJ.do.doador %in% cnpjs.comites$CPF.CNPJ.do.doador)) %>% 
+  filter(as.numeric(as.character(CPF.CNPJ.do.doador)) > 100000000000) %>% 
+  group_by(Nome.candidato, Nome.doador.corrigido, Sigla..Partido, UF, CPF.CNPJ.do.doador) %>% 
   summarise(
     valor.total.doacoes = sum(valor.doacoes, na.rm=TRUE)
   )
 
-doacoes <- doacoes.empresas %>% 
-  group_by(Nome.do.doador, CPF.CNPJ.do.doador) %>%
-  summarise(
-    valor.total.doacoes = sum(valor.total.doacoes, na.rm=TRUE)
-  )
+doacoes.empresas.totais <- doacoes.empresas %>% 
+  group_by(Nome.doador.corrigido, CPF.CNPJ.do.doador) %>%
+  summarise(valor.total.doacoes = sum(valor.total.doacoes, na.rm=TRUE)) %>% 
+  ungroup() %>% 
+  arrange(-valor.total.doacoes)
+
+top5.empresas <- doacoes.empresas.totais[1:5,]
+
+p <- ggplot(doacoes.empresas.totais, 
+            aes(x = 1:NROW(doacoes.empresas.totais), 
+                y = valor.total.doacoes / 1e6)) + 
+  geom_point(size = 3, alpha = 0.8) +
+  geom_text(data = top5.empresas, 
+            aes(x = 1:NROW(top5.empresas), 
+                y = valor.total.doacoes / 1e6, 
+                label = Nome.doador.corrigido), 
+            hjust = -.03, 
+            size = 4, 
+            colour = "grey20") + 
+  ylab("Milhões de reais doados") + 
+  xlab("Ranking do doador") + 
+  theme_bw() + 
+  scale_y_continuous(labels=function(n){format(n, scientific = FALSE)}) + 
+  scale_x_log10()
+p
+png("top-empresas-doadoras.png", 600, 600)
+p
+dev.off()
+
+
+###
+# PCA!
+### 
 
 data.plot <- doacoes[order(desc(doacoes$valor.total.doacoes)),][1:10,]
 
