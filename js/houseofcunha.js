7b460da5af9afbdf6c7db3a4db2289dd4b93dea3
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-08-30 06:18:05 -0300

    dividindo página inicial em seções com rotas angular

--- js/houseofcunha.js
+++ js/houseofcunha.js
@@ -1,1 +1,27 @@
 angular.module('houseofcunha', ['ngRoute']);
+
+angular.module('houseofcunha')
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/ato1', {
+                templateUrl: '/templates/pages/ato1/index.html',
+            })
+            .when('/ato2', {
+                templateUrl: '/templates/pages/ato2/index.html',
+            })
+            .when('/ato3', {
+                templateUrl: '/templates/pages/ato3/index.html'
+            })
+            .when('/ato4', {
+                templateUrl: '/templates/pages/ato4/index.html'
+            })
+            .when('/extras', {
+                templateUrl: '/templates/pages/extras/index.html'
+            })
+            .when('/', {
+                templateUrl: '/templates/pages/ato1/index.html',
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
