7e2c92e7aea0a1be433ff5ce6930f76c0a01fdd4
rodolfoviana <rodolfodelimaviana@gmail.com>
2016-01-27 21:57:45 -0300

    Bug fixed

--- R/camara-lib.R
+++ R/camara-lib.R
@@ -138,7 +138,7 @@ definir_nome <- function(deputados_repetidos,votos) {
 }
 
 # Ler os votos ativos dos deputados
-ler_votos_de_ativos <- function(filepath, corrigir_migracoes, min.porc.votacoes = 0.15, limpar.votos=TRUE){
+ler_votos_de_ativos <- function(filepath, corrigir_migracoes = TRUE, min.porc.votacoes = 0.15, limpar.votos=TRUE){
   votos <- read.csv(filepath, strip.white=TRUE, quote="")
   
   # ajustes nos valores e tipos das variáveis
