bb05ed78718cc579d708cc2662fe0454798a0ded
Rodolfo <rodolfodelimaviana@gmail.com>
2016-04-11 08:11:26 -0300

    Gráfico das afinidades em d3

--- graficos/js/legenda.js
+++ graficos/js/legenda.js
@@ -1,45 +1,44 @@
-var margin = {t:30, r:20, b:20, l:40 },
-    w = 900 - margin.l - margin.r,
-    h = 60 - margin.t - margin.b,
-    x = d3.scale.linear().range([0, w]),
-    y = d3.scale.linear().range([h - 60, 0]),
-//colors that will reflect geographical regions
-    color = d3.scale.ordinal()
-        .domain(["outros","pmdb","psdb","psol","pt"])
-        .range(["#bdbdbd", "darkred", "#0066CC", "#E69F00", "#FF3300"]);
+function legenda() {
+    var marginLegend = {t:0, r:20, b:20, l:75 },
+        w = 900 - marginLegend.l - marginLegend.r,
+        h = 60 - marginLegend.t - marginLegend.b,
+        xLegend = d3.scale.linear().range([0, w]),
+        yLegend = d3.scale.linear().range([h - 60, 0]),
+        //colors that will reflect parties
+        color = d3.scale.ordinal()
+                  .domain(["outros","pmdb","psdb","psol","pt"])
+                  .range(["#bdbdbd", "darkred", "#0066CC", "#E69F00", "#FF3300"]);
 
-var legenda = d3.select("#legenda" +
-    "").append("svg")
-    .attr("width", w + margin.l + margin.r)
-    .attr("height", h + margin.t + margin.b);
+    var legenda = svgWrapper.append("svg")
+        .attr("width", w + marginLegend.l + marginLegend.r)
+        .attr("height", h + marginLegend.t + marginLegend.b);
 
+    // group that will contain all of the plots
+    var groups = legenda.append("g")
+                        .attr("transform", "translate(" + marginLegend.l + "," + marginLegend.t + ")");
 
-// group that will contain all of the plots
-var groups = legenda.append("g").attr("transform", "translate(" + margin.l + "," + margin.t + ")");
+    // array of the parties, used for the legend
+    var parties = ["outros", "pmdb", "psdb", "psol", "pt"]
 
-// array of the regions, used for the legend
-var regions = ["outros", "pmdb", "psdb", "psol", "pt"]
+    // add circles
+    var legend = legenda.selectAll("circle")
+                        .data(parties)
+                        .enter()
+                        .append("circle")
+                        .attr({
+                            cx: function(d, i) { return (75 + i*80); },
+                            cy: (h + marginLegend.t) / 2,
+                            r: 10
+                        })
+                        .style("fill", function(d) { return color(d); });
 
-
-
-var legend = legenda.selectAll("rect")
-    .data(regions)
-    .enter().append("rect")
-    .attr({
-        x: function(d, i) { return (40 + i*80); },
-        y: h,
-        width: 25,
-        height: 12
-    })
-    .style("fill", function(d) { return color(d); });
-
-
-//             legend labels
-legenda.selectAll("text")
-    .data(regions)
-    .enter().append("text")
-    .attr({
-        x: function(d, i) { return (40 + i*80); },
-        y: h + 24,
-    })
-    .text(function(d) { return d; });
+    // legend labels
+    legenda.selectAll("text")
+           .data(parties)
+           .enter().append("text")
+           .attr({
+             x: function(d, i) { return (65 + i*80); },
+             y: ((h + marginLegend.t) / 2) + 20
+            })
+            .text(function(d) { return d; });
+}
