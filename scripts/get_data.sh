b3513723f5dc50034eabc02bc7d76f54dbdef554
Rodolfo <rodolfodelimaviana@gmail.com>
2016-06-27 17:16:05 -0300

    Votações de 2007 e 2008

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,6 +1,8 @@
 #curl --connect-timeout 15 --retry 5 --keepalive-time 10 "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoesVotadasEmPlenario?ano=2015&tipo=" > ../dados/proposicoes-votadas.xml
 #curl --connect-timeout 15 --retry 5 --keepalive-time 10 "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoesVotadasEmPlenario?ano=2016&tipo=" >> ../dados/proposicoes-votadas.xml 
 
+curl --connect-timeout 15 --retry 5 --keepalive-time 10 "http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoesVotadasEmPlenario?ano=2008&tipo="  > ../dados/proposicoes-votadas.xml
+
 
 java -jar xml2csv-conv.jar ../dados/proposicoes-votadas.xml ../dados/proposicoes-votadas.csv
 mkdir ../dados/proposicoes
@@ -8,19 +10,20 @@ for prop in `grep -v rop ../dados/proposicoes-votadas.csv | cut -d, -f1`;
 do 
 	if [ ! -f ../dados/proposicoes/p$prop.xml ]; 
 	then 
-		curl --connect-timeout 15 --retry 5 --keepalive-time 10 'http://www.camara.gov.br//SitCamaraWS/Proposicoes.asmx/ObterProposicaoPorID?idProp='$prop > ../dados/proposicoes/p$prop.xml; 
+		curl --connect-timeout 15 --retry 5 --keepalive-time 10 'http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterProposicaoPorID?idProp='$prop > ../dados/proposicoes/p$prop.xml; 
 		sleep 3;
 	fi
 done
 
-grep '<proposicao ' ../dados/proposicoes/p* | awk -F'"' '{print $2, $4, $6}' > ../dados/props-tipo_numero_ano.txt 
 
+grep '<proposicao ' ../dados/proposicoes/p* | awk -F'"' '{print $2, $4, $6}' > ../dados/props-tipo_numero_ano.txt 
 mkdir ../dados/votacoes
 while read line; 
 do 
 	propnum=`echo $line | awk -F"[ \t\n]+" '{print $2}'`
 	if [ ! -f ../dados/votacoes/$propnum.xml ]; then 
-		propurl=`echo $line | awk -F"[ \t\n]+" '{print "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo=" $1 "&numero=" $2 "&ano=" $3}'`
+		propurl=`echo $line | awk -F"[ \t\n]+" '{print "http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo=" $1 "&numero=" $2 "&ano=" $3}'`
+		echo propurl
 		curl --connect-timeout 15 --retry 5 --keepalive-time 10 $propurl > ../dados/votacoes/$propnum.xml
 		sleep 3;
 	fi 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 echo "GETTING THE DATA"
 sh get_data.sh
-python ./python/parser_deputados.py "../dados/deputados/deputados.xml" "../dados/deputados/deputados.csv"
+python ../python/parser_deputados.py "../dados/deputados/deputados.xml" "../dados/deputados/deputados.csv"
 echo "DOWNLOAD FINISH. NOW WILL TRANSFORM THE DATA"
 python ../python/parser.py "../dados/votacoes/*.xml" "../dados/votacoes.csv"
 python ../python/prop_parser.py "../dados/proposicoes/" "../dados/proposicoes.csv"
